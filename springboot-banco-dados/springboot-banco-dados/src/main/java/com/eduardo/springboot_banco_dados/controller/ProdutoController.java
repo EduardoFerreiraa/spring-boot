e4013cda567d01c5ad557d593bb9f76db0d87212
Eduardoalmeida77 <eduardoferreiraalmeida20@gmail.com>
2025-07-18 18:11:43 -0300

    Configurei o banco de dados, e adicionei dois métodos salvar e listar

--- springboot-banco-dados/springboot-banco-dados/src/main/java/com/eduardo/springboot_banco_dados/controller/ProdutoController.java
+++ springboot-banco-dados/springboot-banco-dados/src/main/java/com/eduardo/springboot_banco_dados/controller/ProdutoController.java
@@ -1,10 +1,33 @@
 package com.eduardo.springboot_banco_dados.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.eduardo.springboot_banco_dados.model.Produto;
+import com.eduardo.springboot_banco_dados.repository.ProdutoRepository;
 
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
     
+    private final ProdutoRepository repository;
+
+    public ProdutoController(ProdutoRepository repository) {
+        this.repository = repository;
+    }
+
+    @PostMapping()
+    public Produto salvar(@RequestBody Produto produto) {
+        return repository.save(produto);
+    }
+
+    @GetMapping
+    public List<Produto> listar() {
+        return repository.findAll();
+    }
 }
