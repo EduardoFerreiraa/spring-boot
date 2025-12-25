a6e7547800b9d5e194d86f63e7f13af498446313
Eduardoalmeida77 <eduardoferreiraalmeida20@gmail.com>
2025-12-25 04:54:32 -0300

    Extraindo

---
+++ primeiro-projeto/primeiro-projeto/src/test/java/eduardo/example/primeiro_projeto/PrimeiroProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package eduardo.example.primeiro_projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
